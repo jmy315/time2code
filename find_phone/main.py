e63446401508bb54514516ac45d102be2005ce39
jmy315 <jmychen315@gmail.com>
2020-08-03 20:07:40 -0700

    Add Find Phone

---
+++ find_phone/main.py
@@ -0,0 +1,25 @@
+"""
+idea:
+1) create RE pattern
+2) read each line and search with pattern
+3) print out valid numbers
+
+complexity: O() where N is
+"""
+
+from absl import app
+import re
+
+def main(argv):
+    find_phone(argv[1])
+
+def find_phone(file):
+    p = '^(([0-9]{3}[\.|\s|-]){2}[0-9]{4})|(\([0-9]{3}\)\s[0-9]{3}[\s|-][0-9]{4})$'
+    with open(file, 'r') as fd:
+        for line in fd:
+            phone = ' '.join(line.split()[2:])
+            if re.match(p, phone):
+                print(phone)
+
+if __name__ == '__main__':
+    app.run(main)
---
+++ find_phone/problem_statement
@@ -0,0 +1,19 @@
+input: a file with the following content
+Rogan Storey	123-456-7890
+Amarah Buxton	123.456.7891
+Reo Farrington	123 456 7892
+Nancy Dalby	(123) 456 7893
+Dillon Barron	(123)456-7894
+Rachelle Small	123 456-7895
+Issa Dunn	123.456.78960
+Ian Dickson	124 456 7897
+Cara Lynn	(123) 456.7898
+Hettie Hudson	(123 456 7899
+ 
+output: valid US phone numbers
+Note: US phone numbers should be in the following formats
+xxx-xxx-xxxx
+xxx.xxx.xxxx
+xxx xxx xxxx
+(xxx) xxx xxxx
+(xxx) xxx-xxxx
