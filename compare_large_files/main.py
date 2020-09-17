8f8cf546345af09db71653ee0794df32b2de5446
jmy315 <jmychen315@gmail.com>
2020-09-17 08:21:18 -0700

    Add Size Check

--- compare_large_files/main.py
+++ compare_large_files/main.py
@@ -7,12 +7,18 @@ idea:
 complexity:O(N) where is the number of lines of the larger file
 """
 
+import os
 import sys
 
 def file_gen(file):
     return((line for line in open(file, 'r')))
 
 def comp(file1, file2):
+    size1 = os.stat(file1).st_size
+    size2 = os.stat(file2).st_size
+    if size1 != size2:
+        return False
+    print("here")
     gen1 = file_gen(file1)
     gen2 = file_gen(file2)
     for a, b in zip(gen1, gen2):
