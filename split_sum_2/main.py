dc17d6b684a7a5c15b177686c6b75c8725da540c
jmy315 <jmychen315@gmail.com>
2020-10-29 20:34:40 -0700

    Add Split Sum 2

---
+++ split_sum_2/main.py
@@ -0,0 +1,27 @@
+"""
+ideas:
+1) get the sum of whole list
+2) left_sum and right_sum accumulate the partial sum
+3) keep a varialbe to keep track of min_diff between left and right sum
+
+complexity: O(N) where N is the number of items in array
+"""
+
+def split_sum_2(array):
+    total = sum(array)
+    left_sum = 0
+    right_sum = total
+    min_diff = abs(left_sum - right_sum)
+    min_index = 0
+
+    for i, num in enumerate(array):
+        left_sum += num
+        right_sum -= num
+        if min_diff > abs(left_sum - right_sum):
+            min_diff = abs(left_sum - right_sum)
+            min_index = i
+    print(array[:min_index+1], array[min_index+1:])
+
+
+split_sum_2([2,7,9,4,6,1])
+split_sum_2([2,7,9,461])
